42792293b0c13673026d982990c3d60bd4ea88d5
Zach Cullen <zacullen@gmail.com>
2020-05-11 15:36:24 -0500

    add functino handleBodyClick to call auth submitform

--- front-end/js/services/listener.js
+++ front-end/js/services/listener.js
@@ -1,10 +1,18 @@
 // attaches listeners to dom elements after content loaded (see index.js)
 class Listener {
-  // handles click events on body elements by identifying target and calling functions
+
+  // handles all click events on body element and calls event handler function
   static listenForClicks() {
     const body = document.querySelector("body")
-    body.addEventListener("click", (e) => {
-      console.log(`clicked on ${e.target.id}!`)
-    })
+    body.addEventListener("click", (e) => this.handleBodyClick(e))
+  }
+
+  static handleBodyClick(e) {
+    switch(e.target.id) {
+      case "login-form-submit":
+        e.preventDefault()
+        Auth.submitLoginForm()
+        break
+    }
   }
 }
