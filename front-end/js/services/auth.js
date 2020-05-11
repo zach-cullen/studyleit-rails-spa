a4748a1b79d56bad3276a5bdabd72d7b4fa94fd5
Zach Cullen <zacullen@gmail.com>
2020-05-11 15:37:22 -0500

    add id to submit button and create callback function for click

--- front-end/js/services/auth.js
+++ front-end/js/services/auth.js
@@ -2,13 +2,19 @@
 // communicates to server about session cookie
 class Auth {
 
+  static submitLoginForm() {
+    console.log("Auth is sumitting form!")
+    // send post request to login with form data and handle promise
+    // if valid user, create user object and hang on to cookie
+  }
+
   // provides html string that can be used to add html form to the dom
   static get viewLoginForm() {
     return `
     <form class="auth-form" id="login-form" action="#" method="post">
       <input id="login-form-input-email" type="text" name="email" value="" placeholder="email">
       <input id="login-form-input-password" type="password" name="password" value="" placeholder="password">
-      <input type="submit" value="Log In">
+      <input id="login-form-submit" type="submit" value="Log In">
     </form>
     `
   }
