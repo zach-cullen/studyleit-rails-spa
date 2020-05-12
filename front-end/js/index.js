cbe40c37e5eec4c7acac4123034c3146c6d4695b
Zach Cullen <zacullen@gmail.com>
2020-05-12 15:55:00 -0500

    chain dom render to after get current user

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -2,14 +2,8 @@ document.addEventListener("DOMContentLoaded", init)
 
 function init() {
   // get current user
-  // triggers 2nd render of main container after fetch current user
-  Auth.getCurrentUser()
-
-  // renders content in main
-  // this will happen before current_user is done setting current user
-  DOM.renderMainContainer()
+  Auth.getCurrentUser().then(DOM.renderMainContainer)
 
   // listen for events
   Listener.listenForClicks()
-
 }
