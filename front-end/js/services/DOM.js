a3cc34390689a938f3d8d6d0ce7cafc368c979f5
Zach Cullen <zacullen@gmail.com>
2020-05-12 12:30:28 -0500

    add console log to investigate timing

--- front-end/js/services/DOM.js
+++ front-end/js/services/DOM.js
@@ -2,6 +2,7 @@
 
 class DOM {
   static renderMainContainer() {
+    console.log("rendering container!!!")
     const main = document.getElementById("main")
     if (Auth.isSignedIn) {
       main.innerHTML = `<h3>Logged in with ${Auth.currentUser.email}</h3>`
