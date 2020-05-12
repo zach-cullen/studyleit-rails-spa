517c8e49e322c060b5a37c66bad629e9ab6bd2da
Zach Cullen <zacullen@gmail.com>
2020-05-12 12:28:29 -0500

    add get_current_user route

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :users
       post "/login", to: "sessions#create"
       post "/logout", to: "sessions#destroy"
+      get "/get_current_user", to: "sessions#get_current_user"
     end
   end
 end
