f31714c801c3a7bef1dbc0f47f2c2df0c886650d
Zach Cullen <zacullen@gmail.com>
2020-05-12 09:07:13 -0500

    clean up user auth and render status 401 if failed

--- rails-api/app/controllers/api/v1/sessions_controller.rb
+++ rails-api/app/controllers/api/v1/sessions_controller.rb
@@ -2,20 +2,23 @@ class Api::V1::SessionsController < ApplicationController
 
   # handles route post /login
   def create
-    user = User.find_by(email: params[:user][:email])
-    if user && user.authenticate(params[:user][:password])
+    #validate that user exists and authenticate password, else user is nil
+    user = User
+            .find_by(email: params[:user][:email])
+            .try(:authenticate, params[:user][:password])
+
+    if user
       #create new session on rails side
+      binding.pry
       session[:user_id] = user.id
+      #send json response with user data
       render json: {
         user: user.as_json(except: [:password_digest, :created_at, :updated_at]),
         logged_in: true
       }
-      #send credentials to JavaScript
     else 
       #return error object
-      render json: {
-        error: "invalid credentials"
-      }
+      render json: { status: 401 }
     end
   end
 
