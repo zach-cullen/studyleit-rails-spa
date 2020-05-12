76169527b390867861be04d0b053487b88998739
Zach Cullen <zacullen@gmail.com>
2020-05-12 10:21:24 -0500

    remove console logs no longer needed

--- front-end/js/services/API.js
+++ front-end/js/services/API.js
@@ -6,13 +6,11 @@ class API {
   static baseURL = "http://localhost:3000/api/v1"
 
   static post(url, body) {
-    console.log(url, body)
     return fetch(this.baseURL + url, {
       method: "POST",
       headers: this.headers,
       body: JSON.stringify(body)
     })
     .then(resp => resp.json())
-    .then(json => console.log(json))
   }
 }
