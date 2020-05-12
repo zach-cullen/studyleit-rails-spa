e194e5f06d0e2b29e31cfe6827636ef7f26ddaac
Zach Cullen <zacullen@gmail.com>
2020-05-12 15:54:27 -0500

    change log message

--- front-end/js/services/DOM.js
+++ front-end/js/services/DOM.js
@@ -1,8 +1,10 @@
 // makes changes to the DOM
 
 class DOM {
+
+  // be careful with THIS because it will get lost in chained method
   static renderMainContainer() {
-    console.log("rendering container!!!")
+    console.log("RENDER main container")
     const main = document.getElementById("main")
     if (Auth.isSignedIn) {
       main.innerHTML = `<h3>Logged in with ${Auth.currentUser.email}</h3>`
