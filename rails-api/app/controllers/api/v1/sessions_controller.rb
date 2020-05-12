cfbe5914e853f827c075b7d21eeaa9cce4a6de2b
Zach Cullen <zacullen@gmail.com>
2020-05-12 08:20:17 -0500

    validate user credentials in session create and return json object

--- rails-api/app/controllers/api/v1/sessions_controller.rb
+++ rails-api/app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,22 @@
 class Api::V1::SessionsController < ApplicationController
+
+  # handles route post /login
   def create
-    binding.pry
-    # handles route post /login
-    # validate login credentials then add to session cookie for use by helper methods in ApplicationController
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      #create new session on rails side
+      session[:user_id] = user.id
+      render json: {
+        current_user: user.as_json(except: [:password_digest, :created_at, :updated_at]),
+        logged_in: true
+      }
+      #send credentials to JavaScript
+    else 
+      #return error object
+      render json: {
+        error: "invalid credentials"
+      }
+    end
   end
 
   def destroy
