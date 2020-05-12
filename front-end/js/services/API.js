58f8074fde22fac1b301da1f9b947e7891ab7d32
Zach Cullen <zacullen@gmail.com>
2020-05-12 12:31:07 -0500

    add static function get

--- front-end/js/services/API.js
+++ front-end/js/services/API.js
@@ -1,7 +1,11 @@
 class API {
   static headers = {
     "Content-Type": "application/json",
-    "Accept": "application/json"
+    "Accept": "application/json",
+  }
+
+  static options = {
+    credentials: "include"
   }
   static baseURL = "http://localhost:3000/api/v1"
 
@@ -9,7 +13,17 @@ class API {
     return fetch(this.baseURL + url, {
       method: "POST",
       headers: this.headers,
+      ...this.options,
       body: JSON.stringify(body)
+    })
+    .then(resp => resp.json())
+  }
+
+  static get(url) {
+    return fetch(this.baseURL + url, {
+      method: "GET",
+      headers: this.headers,
+      ...this.options
     })
     .then(resp => resp.json())
   }
