b089b2affb1836ac2def6ef1a157f598fa07ea82
Zach Cullen <zacullen@gmail.com>
2020-05-12 11:26:58 -0500

    renderMainContainer conditional on user signed in

--- front-end/js/services/DOM.js
+++ front-end/js/services/DOM.js
@@ -3,6 +3,11 @@
 class DOM {
   static renderMainContainer() {
     const main = document.getElementById("main")
-    main.innerHTML = Auth.viewLoginForm
+    if (Auth.isSignedIn) {
+      main.innerHTML = `<h3>Logged in with ${Auth.currentUser.email}</h3>`
+    }
+    else {
+      main.innerHTML = Auth.viewLoginForm
+    }
   }
 }
