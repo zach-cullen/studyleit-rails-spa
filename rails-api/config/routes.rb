029cf439ff604a6950f55eecb43c6a6f5e191c2b
Zach Cullen <zacullen@gmail.com>
2020-05-13 08:15:07 -0500

    move decks into namespace

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :users
+      resources :decks
       post "/login", to: "sessions#create"
       post "/logout", to: "sessions#destroy"
       get "/get_current_user", to: "sessions#get_current_user"
