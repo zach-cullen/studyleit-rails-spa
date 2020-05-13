e80c02c2f8fe3408d12bb63b411a6230a83e4231
Zach Cullen <zacullen@gmail.com>
2020-05-13 08:57:50 -0500

    create valid_user before action to protect resources and provide user

--- rails-api/app/controllers/api/v1/decks_controller.rb
+++ rails-api/app/controllers/api/v1/decks_controller.rb
@@ -1,16 +1,10 @@
 class Api::V1::DecksController < ApplicationController
-  
+  before_action :valid_user
+
   def index
-    @user = User.find_by(id: params[:user_id])
-    binding.pry
-    if @user 
-      @decks = @user.decks
-      render json: @decks, except: [:created_at, :updated_at]
-    else
-      render json: {
-        error: "Invalid request"
-      }
-    end
+    #decks protected by before action that also provides @user
+    @decks = @user.decks
+    render json: @decks, except: [:created_at, :updated_at]
   end 
 
   def create
@@ -20,5 +14,17 @@ class Api::V1::DecksController < ApplicationController
   def destroy
 
   end
+
+
+  def valid_user
+    @user = User.find_by(id: params[:user_id])
+    if @user && @user == current_user
+      @user
+    else
+      render json: {error: "You must be logged in to perform this action."}
+    end
+  end
+
+  
 
 end
