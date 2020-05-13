db7e8cd6f50ca31b9b5523bc558ad7d5f8286a60
Zach Cullen <zacullen@gmail.com>
2020-05-13 08:37:54 -0500

    render deck index based on user param

--- rails-api/app/controllers/api/v1/decks_controller.rb
+++ rails-api/app/controllers/api/v1/decks_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::DecksController < ApplicationController
   
   def index
-    decks = Deck.all
-    render json: decks, except: [:created_at, :updated_at]
+    @user = User.find_by(id: params[:user_id])
+    binding.pry
+    if @user 
+      @decks = @user.decks
+      render json: @decks, except: [:created_at, :updated_at]
+    else
+      render json: {
+        error: "Invalid request"
+      }
+    end
   end 
-
-  def show
-
-  end
 
   def create
 
