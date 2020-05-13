a3100bd636a88ecd77ccb436f93a87c73596d3e0
Zach Cullen <zacullen@gmail.com>
2020-05-13 08:15:40 -0500

    migrate decks table

--- rails-api/db/schema.rb
+++ rails-api/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_183304) do
+ActiveRecord::Schema.define(version: 2020_05_13_130807) do
+
+  create_table "decks", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
