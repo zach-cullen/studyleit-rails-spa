6498ea82ed7e36b2d8600eb8c2144fc849d2fe39
Zach Cullen <zacullen@gmail.com>
2020-05-13 13:21:47 -0500

    add methods for generating views of all dash decks

--- front-end/js/components/dashboard.js
+++ front-end/js/components/dashboard.js
@@ -4,12 +4,29 @@ class Dashboard {
   static get viewDash() {
     return `
     <div class="dashboard" id="dash-outer">
-      <div class="dashboard" id="dash-decks"></div>
+      <div class="dashboard" id="dash-decks">
+        ${this.viewAllDashDecks}
+      </div>
       <div class="dashboard" id="dash-sidebar">
         ${Forms.viewNewDeckForm}
       </div>
    </div>`
   }
 
+  static viewDashDeck(deck) {
+    return `
+    <div class="dash-deck" id="dash-deck-${deck.id}">
+      <div class="dash-deck-title">${deck.title}</div>
+      <div class="dash-deck-tabs"></div>
+    </div>
+    `
+  }
+
+  //returns html string of dash deck views generated from collection of decks saved in content store
+  static get viewAllDashDecks() {
+    console.log("Dashboard creating views for all decks...")
+    return Content.allDecks.map((deck) => this.viewDashDeck(deck)).join("")
+  }
+  
 }
 
