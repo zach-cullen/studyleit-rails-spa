d0ee5ceaf775a6b0f0fa980b7b8d08e07f059401
Zach Cullen <zacullen@gmail.com>
2020-05-13 16:17:10 -0500

    define create action and strong params from deck

--- rails-api/app/controllers/api/v1/decks_controller.rb
+++ rails-api/app/controllers/api/v1/decks_controller.rb
@@ -1,21 +1,30 @@
 class Api::V1::DecksController < ApplicationController
+
+  #decks protected by before action that also provides @user
   before_action :valid_user
 
   def index
-    #decks protected by before action that also provides @user
     @decks = @user.decks
     render json: @decks, except: [:created_at, :updated_at]
   end 
 
   def create
+    @deck = @user.decks.build(deck_params)
 
+    if @deck.save
+      render json: @deck, except: [:created_at, :updated_at]
+    else 
+      render json: { errors: @deck.errors.full_messages }
+    end
+    
   end
 
   def destroy
 
   end
 
-
+  #protect deck actions by validating that user in params is same as logged in user
+  #returns a user instance variable for the controller to use in memory
   def valid_user
     @user = User.find_by(id: params[:user_id])
     if @user && @user == current_user
@@ -25,6 +34,11 @@ class Api::V1::DecksController < ApplicationController
     end
   end
 
+  private 
+
+  def deck_params
+    params.require(:deck).permit(:title)
+  end
   
 
 end
