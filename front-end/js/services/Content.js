08eaa1fbe61fffc7c38991992a5ae6ce52e76d5a
Zach Cullen <zacullen@gmail.com>
2020-05-13 13:24:03 -0500

    trigger DOM rerender after retrieving user deck data and saving

--- front-end/js/services/Content.js
+++ front-end/js/services/Content.js
@@ -16,10 +16,14 @@ class Content {
   static getUserDecks() {
     console.log("loading CONTENT: user decks...")
     return API.get(`/users/${Auth.currentUser.id}/decks`)
-      .then(json => this.loadUserDecks(json))
+      .then(json => {
+        this.loadUserDecks(json)
+        DOM.renderMainContainer()
+      })
   }
 
   // receives json string and instantiates Deck objects, and saves them in Content store
+  // does not make decks appear in the dom, see Dashboard component for rendering
   static loadUserDecks(json) {
     json.forEach(deckData => {
       const deck = new Deck(deckData)
