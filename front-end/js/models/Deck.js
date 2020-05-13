7b992f8b942d7c72b953c8babe466576103407f1
Zach Cullen <zacullen@gmail.com>
2020-05-13 12:17:05 -0500

    define constructor and save method

--- front-end/js/models/Deck.js
+++ front-end/js/models/Deck.js
@@ -1,6 +1,14 @@
 class Deck {
   constructor(attributes) {
     this.id = attributes.id
-    this.email = attributes.email
+    this.title = attributes.title
+    this.user_id = attributes.user_id
+  }
+
+  save(deck) {
+    //type check, then add to content collection
+    if (deck instanceof Deck) {
+      Content.allDecks.push(deck)
+    }
   }
 }
