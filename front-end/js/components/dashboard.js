9a1485a64fa584a420a818a35891caa2c4cbcef7
Zach Cullen <zacullen@gmail.com>
2020-05-13 09:16:44 -0500

    create dashboard class for providing html views to DOM

---
+++ front-end/js/components/dashboard.js
@@ -0,0 +1,13 @@
+// provides html strings to DOM for rendering changes
+
+class Dashboard {
+  static get viewDash() {
+    return `
+    <div class="dashboard">
+      <div class="dashboard" id="dash-decks"></div>
+      <div class="dashboard" id="dash-sidebar"></div>
+   </div>`
+  }
+
+}
+
