72e3a71f63afb7c92b3c74120acbbf97725a8f45
Zach Cullen <zacullen@gmail.com>
2020-05-14 13:41:24 -0500

    move cards controller to api v1 namespace

---
+++ rails-api/app/controllers/api/v1/cards_controller.rb
@@ -0,0 +1,18 @@
+class CardsController < ApplicationController
+  #create validation before action
+  #expect nested routes with user_id an deck_id
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def card_params
+
+  end
+end
