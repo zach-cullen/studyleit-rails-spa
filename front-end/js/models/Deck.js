04e701c7f64176632fcdef9c73261b033d4d0a3e
Zach Cullen <zacullen@gmail.com>
2020-05-14 14:45:58 -0500

    add card storage to Deck model and functions for saving cards

--- front-end/js/models/Deck.js
+++ front-end/js/models/Deck.js
@@ -3,6 +3,22 @@ class Deck {
     this.id = attributes.id
     this.title = attributes.title
     this.user_id = attributes.user_id
+    // always construct with cards empty and add back in from Card save
+    this.cards = []
+  }
+
+  saveCardToDeck(card) {
+    if (card instanceof Card) {
+      this.cards.push(card)
+    } else {
+      console.log("this function will only save Card objects to the deck")
+    }
+  }
+
+  saveCardsFromJson(cards) {
+    cards.forEach((attributes) => {
+      this.saveCardToDeck(new Card(attributes))
+    })
   }
 
   // adds instance of Deck to collection for memory persistence in Content class
