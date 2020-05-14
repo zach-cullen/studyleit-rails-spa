df0574c1e1bfc217b5b45cdeb29ced2a1445f597
Zach Cullen <zacullen@gmail.com>
2020-05-14 14:44:17 -0500

    create Card class

---
+++ front-end/js/models/card.js
@@ -0,0 +1,8 @@
+class Card {
+  constructor(attributes) {
+    this.id = attributes.id
+    this.deck_id = attributes.deck_id
+    this.question = attributes.question
+    this.answer = attributes.answer
+  }
+}
