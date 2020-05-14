f5ea4c178e87a3d6979ba0ec74aa1ae66d123a50
Zach Cullen <zacullen@gmail.com>
2020-05-14 16:19:28 -0500

    dom uses state to render main instead of its own logic

--- front-end/js/services/dom.js
+++ front-end/js/services/dom.js
@@ -3,13 +3,6 @@ class DOM {
 
   static renderMainContainer() {
     const main = document.getElementById("main")
-    if (Auth.isSignedIn) {
-      // render dashboard 
-      main.innerHTML = Dashboard.viewDash
-    }
-    else {
-      // render login
-      main.innerHTML = Forms.viewLoginForm
-    }
+    main.innerHTML = State.renderCurrentView()
   }
 }
