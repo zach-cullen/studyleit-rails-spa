d2b57fe1a10e1e69d41969524d7ac91073a7a9b7
Zach Cullen <zacullen@gmail.com>
2020-05-14 09:30:57 -0500

    flag function for refactoring with comment

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", init)
 
 function init() {
   // get current user triggers load of user decks and render of main container based on response
+  // ** REFACTOR to loadCurrentUserData so that data retrieved on first request
   Auth.getCurrentUser()
   // listen for events
   Listener.listenForClicks()
