726380b76e117a63d63cc4d2332823f1967a9f01
Zach Cullen <zacullen@gmail.com>
2020-05-14 09:25:28 -0500

    move dom rerender to after content load

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", init)
 
 function init() {
-  // get current user triggers load of user decks
-  Auth.getCurrentUser().then(DOM.renderMainContainer)
+  // get current user triggers load of user decks and render of main container based on response
+  Auth.getCurrentUser()
   // listen for events
   Listener.listenForClicks()
 }
