bed7df040b476993d0a7563a6012a07b0edad9f4
Zach Cullen <zacullen@gmail.com>
2020-05-14 13:33:57 -0500

    migrate cards table

--- rails-api/db/schema.rb
+++ rails-api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_130807) do
+ActiveRecord::Schema.define(version: 2020_05_14_181139) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "deck_id"
+    t.string "question"
+    t.string "answer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "decks", force: :cascade do |t|
     t.string "title"
