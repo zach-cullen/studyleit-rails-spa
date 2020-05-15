7250f13721f871ee0360b07f37cd67a2178f158d
Zach Cullen <zacullen@gmail.com>
2020-05-15 08:59:17 -0500

    add content divs to editor view

--- front-end/js/components/editor.js
+++ front-end/js/components/editor.js
@@ -4,7 +4,14 @@ class Editor {
   static viewDeckEditor() {
     const deck = Content.allDecks.find((d) => d.id == State.currentView.id)
     return `
-      <h1>Editing ${deck.title}</h1>
+        <div class="main-inner" id="editor-outer">
+          <div class="main-content-area" id="editor-cards">
+            <h1>${deck.title}</h1>
+          </div>
+          <div class="main-sidebar" id="editor-sidebar">
+            ${Forms.viewNewDeckForm}
+          </div>
+       </div>
     `
   }
 
