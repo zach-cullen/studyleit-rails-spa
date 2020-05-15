8e7b40f58e84c8c116e5b2986c57ec541ca051f0
Zach Cullen <zacullen@gmail.com>
2020-05-15 11:12:51 -0500

    add namespacing to cards controller

--- rails-api/app/controllers/api/v1/cards_controller.rb
+++ rails-api/app/controllers/api/v1/cards_controller.rb
@@ -1,9 +1,9 @@
-class CardsController < ApplicationController
+class Api::V1::CardsController < ApplicationController
   #create validation before action
   #expect nested routes with user_id an deck_id
 
   def create
-
+    binding.pry
   end
 
   def destroy
