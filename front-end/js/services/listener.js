42292e34dcab996f51bdb8ae71af0b05b1a5afd3
Zach Cullen <zacullen@gmail.com>
2020-05-15 13:55:40 -0500

    remove console logs for debugging

--- front-end/js/services/listener.js
+++ front-end/js/services/listener.js
@@ -29,12 +29,9 @@ class Listener {
         this.handleDashDeckTabClick(e)
         break
       case "main-title-back":
-        console.log("CLICKED BACK!")
         State.setViewToDashboard()
         DOM.renderMainContainer()
         break
-      default:
-        console.log(e.target)
     }
   }
 
