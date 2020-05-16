642ec641b031a610ac10968e50d803dcb118d6d3
Zach Cullen <zacullen@gmail.com>
2020-05-16 11:43:44 -0500

    create PracticeSession constructor with makeShuffler method implementing Fisher-Yates Algorithm

--- front-end/js/models/practice-session.js
+++ front-end/js/models/practice-session.js
@@ -2,6 +2,40 @@ class PracticeSession {
 
   constructor(cardCount) {
     this.cardCount = cardCount
+    this.currentCardIndex = 0
+    this.correctCardsCount = 0
+    this.shuffler =  PracticeSession.makeShuffler(cardCount)
+  }
+
+
+  // returns an array of randomized integers representing indexes in array of cards of cardCount length
+  static makeShuffler(cardCount) {
+    // generate array of ints representing indexes of card array of cardCount length
+    const arr = Array.from(Array(cardCount).keys())
+    // implement Fisher-Yates algorithm 
+    // ref: https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+    function shuffleArray(a) {
+      let array = a
+      // start at the end of array and count down
+      for (let i = array.length - 1; i > 0; i--) {
+        // generate a random number between 0 and i - 1
+        // this number represents the index of a random position in an array of length i
+        const j = Math.floor(Math.random() * i)
+        // store the value at i of our original array because it's about to be changed and disappear
+        const temp = array[i]
+        // set the current value at i in our base array to the value at random index j
+        array[i] = array[j]
+        // set the value at random position j to the value we captured from base index
+        // effectively, the two values have now 'swapped'
+        // in the next loop, the random value we 'swapped out' will not be available for exchanging 
+        // because our random integer selection decreases as we count down to no longer include the index it has moved to.
+        // The number that was at position i is still in the mix because it was 'swapped in' to a lower index
+        array[j] = temp
+      }
+      return array
+    }
+
+    return shuffleArray(arr)
   }
 
 }
